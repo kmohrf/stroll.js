04c47f54b3972be29c3e972b423831ff2bc9781d
Konrad Mohrfeldt <mohrfeldt@silicann.com>
2016-06-08 18:51:21 +0200

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # stroll.js
 stroll.js is strolling along in your viewport
+
+## Usage
+
+```es6
+import stroll from "stroll.js";
+
+stroll(".target");
+```
+
+stroll returns native Promises. It resolves, once scrolling is complete or rejects
+if it was cancelled (by calling stroll again while it’s strolling along).
+
+stroll’s first argument can be a number (absolute position in viewport), a selector string
+or something that has a getBoundingClientRect function.
+
+stroll’s second argument can be an object, with one or more of the following properties:
+
+### duration (number, 500)
+time in milliseconds
+ 
+### offset (number, 0)
+is added to the final scroll position. pass a negative number, if you want some space for your fixed header ;)
+
+### focus (bool, true)
+should the element you scroll to be focused once scrolling is done? this keeps keyboard navigation intact.
+  
+### easing (function, easeInOutQuad from [Robert Penner](http://robertpenner.com/easing/))
+timing function
+
+## Shout-out
+
+... goes to jump.js for inspiring me to create my own implementation. They’ve worked on v1.0.0,
+when I created this, and I wasn’t happy with 0.4.0 :).
---
+++ index.js
@@ -0,0 +1,126 @@
+let current_stroll;
+
+function create_options(options) {
+    return Object.assign({
+        offset: 0,
+        duration: 500,
+        focus: true,
+        // Robert Penner's easeInOutQuad - http://robertpenner.com/easing/
+        easing: (t, b, c, d) => {
+            t /= d / 2;
+            if(t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        }
+}, stroll.DEFAULTS, options, {
+        start: window.scrollY || window.pageYOffset
+    });
+}
+
+function resolve_target(user_target, start) {
+    const type = typeof user_target;
+    const target = type === "string" ? document.querySelector(user_target) : user_target;
+    const result = {};
+
+    if(type === "number") {
+        result.target = user_target - start;
+        result.focus = false;
+        return result;
+    }
+
+    if(!target || !target.getBoundingClientRect) {
+        throw new Error("invalid target");
+    }
+
+    result.element = target;
+    result.target =  target.getBoundingClientRect().top;
+
+    return result;
+}
+
+function resolve_duration(duration, distance) {
+    const type = typeof duration;
+
+    if(type === "number") return duration;
+    if(type === "function") return duration(distance);
+
+    const parsed = parseFloat(duration);
+
+    if(isNaN(parsed)) {
+        throw Error("invalid duration");
+    }
+
+    return parsed;
+}
+
+function create_loop(options) {
+    function start_loop(resolve, reject) {
+        let animation_frame;
+        let time_start;
+
+        function loop(time_current) {
+            if(!time_start) {
+                time_start = time_current
+            }
+
+            const time_elapsed = time_current - time_start;
+            const new_pos = options.easing(time_elapsed, options.start, options.target, options.duration);
+            window.scrollTo(0, new_pos);
+
+            if(time_elapsed < options.duration) {
+                next();
+            } else {
+                done(resolve);
+            }
+        }
+
+        function next() {
+            animation_frame = requestAnimationFrame(loop);
+        }
+
+        if(current_stroll) {
+            current_stroll();
+        }
+
+        next();
+
+        return current_stroll = () => {
+            cancelAnimationFrame(animation_frame);
+            reject();
+        }
+    }
+
+    function done(resolve) {
+        current_stroll = null;
+
+        // easing might create small offsets from the requested target
+        window.scrollTo(0, options.start + options.target);
+
+        // keyboard navigation might reset the scroll position
+        // this sets focus to the element to prevent such problems
+        if(options.element && options.focus) {
+            if(!options.element.hasAttribute("tabindex")) {
+                options.element.setAttribute("tabindex", "-1")
+            }
+
+            options.element.focus()
+        }
+
+        resolve();
+    }
+
+    return { start: start_loop };
+}
+
+function stroll(target, options = {}) {
+    const stroll_options = create_options(options);
+    const stroll_target = resolve_target(target, stroll_options.start, stroll_options.offset);
+    const duration = resolve_duration(stroll_options.duration, Math.abs(stroll_target.target));
+    const loop = create_loop(Object.assign({}, stroll_options, stroll_target, { duration }));
+
+    return new Promise(loop.start);
+}
+
+stroll.DEFAULTS = {};
+
+export default stroll;
